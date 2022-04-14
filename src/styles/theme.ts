1fa83236792cc4c67cf2a9380e4db339e1cceb58
neilson <joseneilson25@gmail.com>
2022-04-14 17:39:01 -0300

    pagina de login

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,20 @@
 import { extendTheme } from '@chakra-ui/react'
 
 export const theme = extendTheme({
+    colors: {
+    gray:{
+        "900": "#181823",
+        "800": "#1F2029",
+        "700": "#353646",
+        "600": "#4B4063",
+        "500": "#616438",
+        "400": "#79709A",
+        "300": "#96498E",
+        "200": "#8395ce",
+        "100": "#010200",
+        "50": "#EEEEEF2",
+        }
+    },
     fonts:{
         heading: 'Roboto',
         body: 'Roboto',
