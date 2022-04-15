b88c348d54022b2937c91e68529778a10437ccfb
neilson <joseneilson25@gmail.com>
2022-04-15 18:25:49 -0300

    sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,17 @@
 import Header from "../components/Header";
+import { Flex } from "@chakra-ui/react";
+import { Sidebar } from "../components/Sidebar";
 
 export default function Dashboard(){
     return(
-        <Header />
+        <Flex direction="column" height="100vh">
+            <Header />
+
+                <Flex width="100%" marginY="6" maxWidth={1480} marginX="auto"
+                paddingX="6">
+                <Sidebar />
+
+            </Flex>
+        </Flex>
     );
 } 
