615b1b93dd576a8d16364432feb7db0b3994c4e3
neilson <joseneilson25@gmail.com>
2022-04-17 21:31:44 -0300

    dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,64 @@
 import Header from "../components/Header";
-import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
 import { Sidebar } from "../components/Sidebar";
+import dynamic from "next/dynamic";
+
+const Chart = dynamic(() => import("react-apexcharts"),{
+    ssr: false,
+})
+
+const options = {
+    chart: {
+      toolbar:{
+         show: false,
+      },
+      zoom: {
+          enabled: false,
+      },
+      forColor: theme.colors.gray[500],
+    },
+    grid: {
+        show: false,
+    },
+    dataLabels: {
+        enabled: false,
+    },
+    tooltip: {
+        enabled: false,
+    },
+    xaxis: {
+       type: 'datetime',
+       axisborder:{
+           color: theme.colors.gray[600]
+       },
+       axisTics: {
+        color: theme.colors.gray[600]
+       },
+       categories:[
+        '2022-03-01T00:00:00.000Z',
+        '2022-03-02T00:00:00.000Z',
+        '2022-03-03T00:00:00.000Z',
+        '2022-03-04T00:00:00.000Z',
+        '2022-03-05T00:00:00.000Z',
+        '2022-03-06T00:00:00.000Z',
+        '2022-03-07T00:00:00.000Z',
+       ]      
+    },
+    fill: {
+        opacity: 0.3,
+        type: 'gradient',
+        gradient: {
+            shade: 'dark',
+            opacityFrom: 0.7,
+            opacityTo: 0.3
+        }
+    }
+};
+
+const series = [
+    {name: 'series1', data:[31, 120, 10, 28, 61, 18, 109] }
+]
+
 
 export default function Dashboard(){
     return(
@@ -11,7 +69,7 @@ export default function Dashboard(){
                 paddingX="6">
                 <Sidebar />
 
-                <SimpleGrid flex="1" gap ="4" minChildWidth="320px" align="flex-start">
+                <SimpleGrid flex="1" gap ="4" minChildWidth="320px" >
                     <Box
                         p="8"
                         bg="gray.800"
@@ -19,6 +77,7 @@ export default function Dashboard(){
                            //pb="4"
                            >
                         <Text fontSize="lg" mb="4"> Incritos da semana</Text>
+                        <Chart options={options} series={series} type="area" height={160} />
                     </Box>
                     <Box
                         p="8"
@@ -27,6 +86,7 @@ export default function Dashboard(){
                            //pb="4"
                            >
                         <Text fontSize="lg" mb="4"> Taxa de abertura</Text>
+                        <Chart options={options} series={series} type="area" height={160} />
                     </Box>
                     
                 </SimpleGrid>
