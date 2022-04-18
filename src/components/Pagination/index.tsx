c53eeac67cb34849f81f41e3c4de794e4e46714d
neilson <joseneilson25@gmail.com>
2022-04-17 23:47:53 -0300

    respnsividade chakra

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -4,12 +4,11 @@ import { PaginationItem } from "./PaginationItem";
 export function Pagination(){
     return(
         <Stack
-        direction={['column','row']}
-        mt="8"
-        justify='space-between'
-        align='center'
-        spacing="6"
-        >
+            direction={['column','row']}
+            mt="8"
+            justify='space-between'
+            align='center'
+            spacing="6">
             <Box>
                 <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
             </Box>
